296e58ad2e0cc65a33977e2c502492a4f5d1ddc1
Andreas Gerlach <andi@appelgriebsch.org>
2018-05-11 12:32:20 +0200

    add initial config for OniVim

---
+++ onivim/config.tsx
@@ -0,0 +1,44 @@
+
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+    console.log("config deactivated")
+}
+
+export const configuration = {
+    //add custom config here, such as
+
+    "autoUpdate.enabled": true,
+   
+    "ui.colorscheme": "nord",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    
+    //"editor.fontSize": "12px",
+    //"editor.fontFamily": "Monaco",
+    "editor.fontSize": "14px",
+    "editor.fontFamily": "'Fira Code', Hack, Monaco",
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+}
