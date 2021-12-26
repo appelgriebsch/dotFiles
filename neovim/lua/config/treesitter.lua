2945cdd80ac17e8ae4ae3aabc72be1d7923dafe4
Andreas Gerlach <andi@appelgriebsch.org>
2021-12-26 10:42:49 +0100

    markdown treesitter part of default setup now

--- neovim/lua/config/treesitter.lua
+++ neovim/lua/config/treesitter.lua
@@ -1,12 +1,6 @@
 -- Add Markdown
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.jsonc.used_by = "json"
-parser_config.markdown = {
-  install_info = {
-    url = "https://github.com/ikatyang/tree-sitter-markdown",
-    files = { "src/parser.c", "src/scanner.cc" },
-  },
-}
 
 require('nvim-treesitter.configs').setup({
   ensure_installed = {
