c6196060790bff47f12c486874ba8f0cd3c037fd
Andreas Gerlach <andreas.gerlach@xarvio.com>
2022-02-24 13:09:05 +0100

    add lua config for nvim specific apis

--- neovim/lua/config/lsp-installer.lua
+++ neovim/lua/config/lsp-installer.lua
@@ -21,6 +21,24 @@ lsp_installer.on_server_ready(function(server)
 
     local opts = lsp_setup.make_config()
 
+		if server.name == "sumneko_lua" then
+			-- only apply these settings for the "sumneko_lua" server
+			opts.settings = {
+				Lua = {
+					diagnostics = {
+						-- Get the language server to recognize the 'vim', 'use' global
+						globals = {'vim', 'use', 'require'},
+					},
+					workspace = {
+						-- Make the server aware of Neovim runtime files
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+					-- Do not send telemetry data containing a randomized but unique identifier
+					telemetry = {enable = false},
+				},
+			}
+		end
+
     if server.name == "jsonls" then
       opts.settings = {
         json = {
@@ -36,7 +54,7 @@ lsp_installer.on_server_ready(function(server)
             includeInlayEnumMemberValueHints = true,
             includeInlayFunctionLikeReturnTypeHints = true,
             includeInlayFunctionParameterTypeHints = true,
-            includeInlayParameterNameHints = "all", -- none | literals | all      
+            includeInlayParameterNameHints = "all", -- none | literals | all
             includeInlayParameterNameHintsWhenArgumentMatchesName = true,
             includeInlayPropertyDeclarationTypeHints = true,
             includeInlayVariableTypeHints = true,
