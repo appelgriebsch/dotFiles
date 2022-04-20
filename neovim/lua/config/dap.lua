b184c6066f37c9e7c78146d49ea48890365d7854
Andreas Gerlach <andreas.gerlach@xarvio.com>
2022-04-20 15:18:31 +0200

    make dap icons more recognizable

--- neovim/lua/config/dap.lua
+++ neovim/lua/config/dap.lua
@@ -2,10 +2,10 @@ local dap = require('dap')
 dap.defaults.fallback.terminal_win_cmd = '1split new'
 require("nvim-dap-virtual-text").setup()
 
-vim.fn.sign_define("DapBreakpoint", { text="", texthl="", linehl="", numhl="" })
-vim.fn.sign_define("DapBreakpointCondition", { text="", texthl="", linehl="", numhl="" })
-vim.fn.sign_define("DapLogPoint", { text="ﱴ", texthl="", linehl="", numhl="" })
-vim.fn.sign_define("DapStopped", { text="", texthl="", linehl="", numhl="" })
+vim.fn.sign_define("DapBreakpoint", { text="", texthl="", linehl="", numhl="" })
+vim.fn.sign_define("DapBreakpointCondition", { text="", texthl="", linehl="", numhl="" })
+vim.fn.sign_define("DapLogPoint", { text="", texthl="", linehl="", numhl="" })
+vim.fn.sign_define("DapStopped", { text="", texthl="", linehl="", numhl="" })
 
 vim.api.nvim_exec([[
   au FileType dap-repl lua require('dap.ext.autocompl').attach()
