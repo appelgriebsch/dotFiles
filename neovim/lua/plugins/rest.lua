af41f4ec80035c217c144595835e1c4ee8c9f67f
Andreas Gerlach <andreas.gerlach@xarvio.com>
2022-07-20 10:58:23 +0200

    set output of REST calls to below

--- neovim/lua/plugins/rest.lua
+++ neovim/lua/plugins/rest.lua
@@ -5,7 +5,7 @@ end
 
 rest.setup({
   -- Open request results in a horizontal split
-  result_split_horizontal = false,
+  result_split_horizontal = true,
   -- Keep the http file buffer above|left when split horizontal|vertical
   result_split_in_place = false,
   -- Skip SSL verification, useful for unknown certificates
