a50cf7c0392c4ca07a855e89ead1d9e829ad2985
Andreas Gerlach <andi@appelgriebsch.org>
2022-12-10 22:35:54 +0100

    use which-key for spell checker

--- neovim/lua/plugins/whichkey.lua
+++ neovim/lua/plugins/whichkey.lua
@@ -8,7 +8,7 @@ whichkey.setup({
     marks = true, -- shows a list of your marks on ' and `
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
-      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
       suggestions = 20, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
